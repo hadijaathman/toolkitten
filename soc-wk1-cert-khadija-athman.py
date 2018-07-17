957a37e43a9552c662386a49f210634e3b4b99d8
hadijaathman <41279094+hadijaathman@users.noreply.github.com>
2018-07-17 16:22:46 +0300

    Create soc-wk1-cert-khadija-athman.py

---
+++ soc-wk1-cert-khadija-athman.py
@@ -0,0 +1,23 @@
+print(1+2)
+print(5*(4*3)*4)
+print(1<<9)
+print(2%4)
+print(2**8)
+print( 24*7)
+print(24*365)
+print(60*24)
+print(60*(365*10))
+print(24*365)
+print(8760*8030)
+print(60*(8760*8030))
+print(60*(60*(8760*8030)))
+#my age in seconds	
+print(365*8640)
+#converting age in seconds to years	
+print(48618000 /3153600)
+#days to time overflow
+print(20*356)
+#calculating my exact age
+from datetime import datetime, timedelta
+age = datetime.now() - datetime.strptime("Sun, 4 fenruary 1996 12:00:32 +0200 ","%a, %d %b %Y %H:%M:%S +0200")
+print (age.days)
