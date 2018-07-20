e8d2e0e18b3b44da80389a865b5a39ae2e56fad3
hadijaathman <41279094+hadijaathman@users.noreply.github.com>
2018-07-20 16:45:16 +0300

    Create soc01hw-khadija-athman.py

---
+++ summer-of-code/week-01/wk1-homework-submission/soc01hw-khadija-athman.py
@@ -0,0 +1,25 @@
+#afew things to try
+#greeting with a name.
+print(str('please enter your first name:'))
+var1 =input( )
+print(str('please enter your middle name:'))
+var2 =input( )
+print(str('please enter your last name:'))
+var3 =input( )
+print('greetings ' + str( var1)   + str( var2)   +  str( var3))
+
+
+#favourite number
+
+print(str('please enter your favourite number:'))
+var4=input()
+var5= print(int(var4) + 1)
+print(str(int(var4)+1 )  +  str( ' is a bigger and better favourite number'))
+
+#rude boss
+
+print('WHAT DO YOU WANT!')
+want=(input())
+print('WHADDAYA MEAN '+str(' "') + str( want.upper() ) +str(' "!?!') + str( ' YOU\'RE FIRED!!!'))
+
+
